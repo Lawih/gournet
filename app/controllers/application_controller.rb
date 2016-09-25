771e3e7979e00cde121138ad281b04ed9dfde3f9
Andrés López <andflopezbec@unal.edu.co>
2016-09-25 05:59:51 -0500

    Configure login and Signup with username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_new_column_to_devise_permitted_parameters, if: :devise_controller?
+
+  private
+  #Insert parameters of user
+  def configure_new_column_to_devise_permitted_parameters
+    registration_params = [:name, :lastname, :username, :email, 
+    	:password, :password_confirmation]
+    if params[:action] == 'create'
+      devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
+    elsif params[:action] == 'update'
+      devise_parameter_sanitizer.permit(:account_update, keys: registration_params)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,38 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  #attr_accessor :login
+  
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  #if you want email to be case insensitive, you should add 
+
+  validates :username , presence: true , confirmation: true
+   # Validamos que el identificador tenga entre 8 a 12 caracteres
+  validates :username, length: { in: 4..100 , message: "debe ser mayor a 4 caracteres"}
+  # Validamos que el email sea unico
+  validates :username, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
+
+  #Validar que nombre de usuario no contenga @
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  # when allowing distinct User records with, e.g., "username" and "UserName"...
+  
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
 end
