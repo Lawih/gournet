ad2965badd1adc111d4a25b41de46606fd7f54ef
jonathan <jomartinezch@gmail.com>
2016-09-26 11:32:01 -0500

    Set up contact form
    1. Add gem 'mail_form', and routes resources :contacts [:new, :create]
    2. create and edit contacts controller
    3. create and edit model, inherits from 'MailForm::Base'
    4. create views, in contacts:
    new # form
    create # callback
    5. set hidden 'spam filter' declare hidden in application.css

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
     resources :dishes
     resources :chefs
     resources :users
+    resources :contacts, only: [:new, :create]
   end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
