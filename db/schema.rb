ada6735eebdf0129872a56795b5fe9e5d707b59b
Andrés López <andflopezbec@unal.edu.co>
2016-11-07 18:12:14 -0500

    Repair show dishes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925174214) do
+ActiveRecord::Schema.define(version: 20161107021328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -189,17 +189,18 @@ ActiveRecord::Schema.define(version: 20160925174214) do
     t.date     "birthday"
     t.string   "phone"
     t.string   "picture"
-    t.string   "type",                   default: "Diner"
+    t.string   "type"
     t.string   "category"
     t.text     "bio"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.string   "email",                  default: "",      null: false
-    t.string   "encrypted_password",     default: "",      null: false
+    t.boolean  "enabled"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,       null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
