63f365d34cf51f7de53c3eb517b3fa291c934ee8
SebastianPardo <jspardoj@unal.edu.co>
2016-11-06 23:18:48 -0500

    Add omiauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+OmniAuth.config.logger = Rails.logger
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '1646406112338578','fbf4cb46d22a85bc582ce70c04347538',:scope => 'user_about_me,user_location, email, user_friends, public_profile' #, :strategy_class => OmniAuth::Strategies::Facebook
+end 
